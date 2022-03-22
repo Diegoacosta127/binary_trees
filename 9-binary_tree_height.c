27fc2fbf8568063956969bced8c53ece42295b7f
ojo-perezoso <bymartin24@gmail.com>
2022-03-22 10:10:19 -0700

    add: 10 binary tree depth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,11 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	h_left = 1 + binary_tree_height(tree->left);
 	h_right = 1 + binary_tree_height(tree->right);
-	
+
 	if (h_left > h_right)
 		return (h_left);
 	else
 		return (h_right);
-	
-	
+
+
 }
